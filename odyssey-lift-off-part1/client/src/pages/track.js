3ab8e5dc53de4119e449c42ec9e5de8ca30a67a7
Alexander Lansdorp <alexander.lansdorp.ext@terpel.com>
2023-03-22 18:39:13 -0500

    liftOff 1 - 4

---
+++ odyssey-lift-off-part1/client/src/pages/track.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { Layout, QueryResult } from '../components';
+import TrackDetail from '../components/track-detail';
+
+export const GET_TRACK = gql`
+	query GetTrack($trackId: ID!) {
+		track(id: $trackId) {
+			id
+			title
+			author {
+				id
+				name
+				photo
+			}
+			thumbnail
+			length
+			modulesCount
+			numberOfViews
+			modules {
+				id
+				title
+				length
+			}
+			description
+		}
+	}
+`;
+
+const Track = ({ trackId }) => {
+	const { loading, error, data } = useQuery(GET_TRACK, {
+		variables: { trackId },
+	});
+	return (
+		<Layout>
+			<QueryResult error={error} loading={loading} data={data}>
+				<TrackDetail track={data?.track} />
+			</QueryResult>
+		</Layout>
+	);
+};
+
+export default Track;
